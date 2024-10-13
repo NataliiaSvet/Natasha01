e70a9b77796366bf48214bb75b7a0461d534fbd2
Nataliia <nsvetashova@email.cz>
2024-10-13 17:31:58 +0200

    Update diagramma.py

--- diagramma.py
+++ diagramma.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
+# Выбор шрифта, поддерживающего кириллицу (например, Arial)
+rcParams['font.family'] = 'Arial'
+
+# Загрузка данных
+df = pd.read_excel('DA_Svietashova_diagramma.xlsx')
+
+# Предположим, что у вас есть столбцы 'Вид помощи' и 'Значения'
+categories = df['Вид помощи']
+values = df['Сумма, крон']
+
+# Построение круговой диаграммы с увеличенным шрифтом и автопроцентами
+plt.figure(figsize=(10, 10))  # Размер графика
+plt.pie(values, labels=categories, autopct='%1.1f%%', textprops={'fontsize': 14}, startangle=45)  # Вращение диаграммы для удобства чтения
+plt.title('Затраты Чешской республики на помощь беженцам из Украины, 2022-2024 гг.', fontsize=18, pad=40)  # Увеличенный заголовок, 
+plt.axis('equal')  # Для того чтобы круг не был эллипсом
+plt.show()
