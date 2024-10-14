574c32986ba9191502e9b8b6a117db40bdfcd27c
Nataliia <nsvetashova@email.cz>
2024-10-14 18:33:37 +0200

    Update diagramma.py

--- diagramma.py
+++ diagramma.py
@@ -19,14 +19,14 @@ st.markdown("<h1 style='text-align: center; color: black;'>Затраты Чеш
 # Загрузка данных
 df = pd.read_excel('DA_Svietashova_diagramma.xlsx')
 
+# Предположим, что у вас есть столбцы 'Вид помощи' и 'Сумма, крон'
+categories = df['Вид помощи']
+values = df['Сумма, крон']
+
 # Добавление строки "Итого"
 total_row = df.sum(numeric_only=True)  # Суммируем только числовые колонки
 total_row.name = 'Итого'  # Название строки
 df = df.append(total_row)  # Добавляем строку в DataFrame
-
-# Предположим, что у вас есть столбцы 'Вид помощи' и 'Сумма, крон'
-categories = df['Вид помощи']
-values = df['Сумма, крон']
 
 # Установка фона через markdown с использованием CSS
 st.markdown(
