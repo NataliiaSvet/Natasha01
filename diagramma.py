11890f27ab2706872f3f2fc638f960114bcebb8d
Nataliia <nsvetashova@email.cz>
2024-10-14 16:59:55 +0200

    Update diagramma.py

--- diagramma.py
+++ diagramma.py
@@ -16,6 +16,18 @@ df = pd.read_excel('DA_Svietashova_diagramma.xlsx')
 categories = df['Вид помощи']
 values = df['Сумма, крон']
 
+# Установка фона через markdown с использованием CSS
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background-color: #e6ffe6; /* Светло-зеленый фон */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Построение круговой диаграммы с увеличенным шрифтом и автопроцентами
 plt.figure(figsize=(10, 10))  # Размер графика
 plt.pie(values, labels=categories, autopct='%1.1f%%', textprops={'fontsize': 14}, startangle=45)  # Вращение диаграммы для удобства чтения
