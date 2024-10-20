feed360b6255e606a789cbe6921b1fe9e6550682
Nataliia <nsvetashova@email.cz>
2024-10-20 16:12:33 +0200

    Update karta.py

--- karta.py
+++ karta.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import streamlit as st
 
-# Загрузите файл через интерфейс Streamlit
+# Добавляем возможность загрузки файла
 uploaded_file = st.file_uploader("Загрузите файл с данными о беженцах", type=["xlsx"])
 
+# Проверяем, загружен ли файл
 if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
-    st.write(df)
-
-
+    df = pd.read_excel(uploaded_file)  # Чтение файла
+    st.write(df)  # Отображение данных файла
 
   
