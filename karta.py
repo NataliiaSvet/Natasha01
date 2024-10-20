4665e21420074ee4de534ae9a00a2007f0fd3a1a
Nataliia <nsvetashova@email.cz>
2024-10-20 16:20:22 +0200

    Update karta.py

--- karta.py
+++ karta.py
@@ -1,13 +1,5 @@
+import streamlit as st
 import pandas as pd
 
-# Замените 'your_file.xlsx' на имя вашего файла
-df = pd.read_excel('DA_Svietashova_karta.xlsx') 
-# Для .xlsx
-# df = pd.read_csv('your_file.csv')    # Для .csv
-
-# Отображение первых 5 строк таблицы
-print(df.head())
-
-# Проверка названий колонок
-print(df.columns)
-
+df = pd.read_excel('DA_Svietashova_karta.xlsx')  # Замените на ваш файл
+st.dataframe(df)
