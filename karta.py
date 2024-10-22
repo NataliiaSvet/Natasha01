4757836e38b727a0ff7cf534bc3e063505ae81dd
Nataliia <nsvetashova@email.cz>
2024-10-22 08:36:30 +0200

    Update karta.py

--- karta.py
+++ karta.py
@@ -48,7 +48,7 @@ ax.set_ylabel('Регион')
 # ax.set_title('Количество беженцев в процентах по регионам')
 
 # Увеличение полей вокруг диаграммы
-plt.subplots_adjust(left=0.2, right=0.98, top=0.9, bottom=0.3)  # Увеличение полей
+plt.subplots_adjust(left=0.2, right=0.9, top=0.9, bottom=0.2)  # Увеличение полей
 
 # Добавление значений на каждый столбик
 for bar in bars:
