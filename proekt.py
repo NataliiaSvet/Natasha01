0a7d95d94dab0dd7d8c200356d394437aff2e307
Nataliia <nsvetashova@email.cz>
2024-10-22 10:12:53 +0200

    Update proekt.py

--- proekt.py
+++ proekt.py
@@ -90,7 +90,7 @@ values = df_refugees['Количество, чел.']
 
 # Построение столбчатой диаграммы
 plt.figure(figsize=(8, 4))  # Установка размера графика
-plt.bar(categories, values, color='Royal Blue', width=0.5)  # Построение графика
+plt.bar(categories, values, color='RoyalBlue', width=0.5)  # Построение графика
 
 # Добавление меток осей
 plt.xlabel('Период времени', fontsize=10)
